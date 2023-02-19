750272f92005ad84e0cabe7092abca8a63251477
LeonidHome <defenser2205@gmail.com>
2023-02-19 15:40:15 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        String str = "hhтоh";
+        if (isPalindrom(str)) {
+            System.out.println("Строка палиндром");
+        } else {
+            System.out.println("Строка не палиндром");
+        }
+    }
 
+    public static boolean isPalindrom(String str) {
+        for (int i = 0, j = str.length() - 1; i < j; i++, j--) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
